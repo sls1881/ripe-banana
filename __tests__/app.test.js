d60a0dd50921733ad47c034860a33b7147096ea9
‘Shelby <sls1881@gmail.com>
2021-04-09 11:40:11 -0700

    Studio get test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,28 @@ const db = require('../lib/utils/database');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Studio = require('../lib/model/Studio');
 
 describe('ripe-banana routes', () => {
   beforeEach(() => {
     return setup(db.sync({ force: true }));
   });
+
+  //Studio
+  it('should get a studio', async() => {
+  const newStudio = await Promise.all({
+    Studio.create = 
+    {id: 1,
+        name: 'MGM',
+        city: 'Los Angeles',
+        state: 'CA',
+        country: 'US'}
+      })
+    
+    return request(app)
+    .get('/api/v1/studios')
+    .then((res) => {
+      expect(res.body).toEqual({id: 1, name: MGM});
+    })
+  })
 });
